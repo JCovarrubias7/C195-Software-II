48d26d0387457fdaee5dd44853f59f44c1c7dca5
JCovarrubias7 <artnhector@gmail.com>
2020-04-02 18:24:49 -0500

    Configure logger to save user logins to text file "loginlog.txt".

--- src/View_Controller/LoginScreenController.java
+++ src/View_Controller/LoginScreenController.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -27,6 +31,9 @@ import javafx.stage.Stage;
  * @author Jorge Covarrubias
  */
 public class LoginScreenController implements Initializable {
+    
+    //Set the logger
+    Logger log = Logger.getLogger("loginlog.txt");
     
     //Declare Fields
     Stage stage;
@@ -60,7 +67,18 @@ public class LoginScreenController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        //Set up logger to write lines to a txt file
+        try {
+        FileHandler fh = new FileHandler("loginlog.txt", true); //if true, fileHandler will append to file
+        SimpleFormatter sf = new SimpleFormatter();
+        fh.setFormatter(sf);
+        log.addHandler(fh);
+        }
+        catch (IOException | SecurityException ex) {
+            Logger.getLogger(LoginScreenController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //Set language based on systemDefault
         setLanguage(); 
     }
 
@@ -84,6 +102,10 @@ public class LoginScreenController implements Initializable {
         }
         else {
             if(checkCredentials(userName, userPassword) == true) {
+                //Write to log a successful log in
+                log.log(Level.INFO, userName + " succesfully logged in.");
+                
+                //Get Main Menu scene
                 stage = (Stage)((Button)event.getSource()).getScene().getWindow();
                 scene = FXMLLoader.load(getClass().getResource("/View_Controller/MainMenu.fxml"));
                 stage.setScene(new Scene(scene));
